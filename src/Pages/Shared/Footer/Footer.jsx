44264ebaf4b7ada71873f9f5baac668c70af0390
shajib1927 <shajibshahrear@gmail.com>
2023-08-25 06:21:07 +0300

    updated

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Footer = () => {
     return (
-        <div>
+        <div className="d-flex justify-content-center align-items-center fixed-bottom">
             <p>Copyright 2023 by Shajib</p>
         </div>
     );
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import logo from "../../../assets/Dragon_Photos/logo.png";
+import moment from "moment/moment";
 
 const Header = () => {
-    return (
-        <div>
-            <h1>This is Header</h1>
+  return (
+    <>
+      <div className="d-flex justify-content-center align-items-center mt-5">
+        <div className="text-center">
+          <img src={logo} alt="logo" />
+          <p className="text-secondary mb-0">
+            Journalism without any fear or favour
+          </p>
+          <p className="text-secondary">
+            {moment().format("MMMM Do YYYY, dddd, h:mm:ss a")}{" "}
+          </p>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Header;
